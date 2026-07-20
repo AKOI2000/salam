8b0ca24d27ed8057aeb92d116aa4670d0c6391da
AKOI2000 <alausayinka@gmail.com>
2026-07-20 18:04:03 +0100

    testimonial section edited, some work needs to be done with the responsiveness though

--- app/_components/TestimonialCard.jsx
+++ app/_components/TestimonialCard.jsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 
-function TestimonialCard() {
+function TestimonialCard({testimonial}) {
   return (
     <div className="card">
       <div className="card-inner">
         <div className="card-front">
           <div className="img-box">
             <Image
-              src={"/Hennessy.png"}
-              alt="hennessy"
+              src={testimonial.imageUrl}
+              alt={`${testimonial.name} as ${testimonial.role}`}
               width={150}
               height={150}
             />
@@ -16,17 +16,13 @@ function TestimonialCard() {
         </div>
         <div className="card-back">
           <p className="card-back_header">
-            There are talented creatives all around, but in all my years of
-            works, I’m yet to find anyone able to express their creativity as
-            well as AjaNwachuku. It doesn’t matter if it’s fintech, govtech or a
-            media product — you’re guaranteed that aj will show up with big
-            ideas, incredible execution and a big, big personality.
+          {testimonial.message}
           </p>
 
           <div className="card-back_info">
             <div className="">
-              <h6>Alausa Olayinka</h6>
-              <p>Gbogbo nitishe</p>
+              <h6>{testimonial.name}</h6>
+              <p>{testimonial.role}</p>
             </div>
           </div>
         </div>
--- app/_sections/Testimonial.jsx
+++ app/_sections/Testimonial.jsx
@@ -1,23 +1,76 @@
 import TestimonialCard from "../_components/TestimonialCard";
 
+const testimonials = [
+  {
+    id: 1,
+    imageUrl: "/imatination.png",
+    name: "Matin",
+    role: "Lead and Creative Director, Imatinations",
+    message:
+      "Mo salam is an exceptional Motion Designer who is incredibly diligent in his work. He is professional and consistently delivers outstanding quality. What impresses me most is his ability to produce top-notch results within a remarkably short turnaround time. I highly recommend him to anyone looking for quality, reliability, and excellence.",
+  },
+  {
+    id: 2,
+    imageUrl: "/hook1.png",
+    name: "Miracle",
+    role: "Brand Manager, The Hook Creative Agency",
+    message:
+      "In my experience at a 360 advertising agency, I have only worked closely with two motion designers, and Salam is at the top of that list. He has an incredible ability to manage multiple projects and accounts at the same time without compromising on quality. We worked together on three different brands, each with unique requirements and the same tight deadlines, and he consistently delivered excellent results.",
+  },
+  {
+    id: 3,
+    imageUrl: "/hook1.png",
+    name: "Ugochukwu",
+    role: "Manager, The Hook Creative Agency",
+    message:
+      "Working with him has been a great experience. He has a strong instinct for brand storytelling and consistently turns ideas into engaging motion graphics and video. He is very creative, open to feedback, and always professional. I genuinely enjoy working with him and would happily recommend for future projects.",
+  },
+  {
+    id: 4,
+    imageUrl: "/gtco.png",
+    name: "Asiwaju",
+    role: "Sales Executive, GTCO",
+    message:
+      "He clearly brings to life your ideas regardless of how vague you present them to him. I think that extra mile he goes to clearly communicate in visuals trumps every other thing for me.",
+  },
+  {
+    id: 5,
+    imageUrl: "/hook1.png",
+    name: "Aisha Adeniyi",
+    role: "Digital Lead, The Hook Creative Agency",
+    message:
+      "Working with Mo Salam has been such an experience. As a brilliant motion designer, he brings energy, creativity, and authenticity to every project. It's always impressive how I can share ideas straight off the top of my head, and Salam is able to bring them to life in ways that often exceed expectations. And yes, his attention to detail is exceptional. He is a reliable team member to work with.",
+  },
+  {
+    id: 6,
+    imageUrl: "/So&u.png",
+    name: "Inomakora",
+    role: "Art Director, So&U",
+    message:
+      "I really enjoyed working with Salam. He’s a creative person who loves and understands his craft. I look forward to working with you again man!",
+  },
+];
+
 function Testimonial() {
   return (
     <section className="container">
       <div className="section-heading">
         <p className="no-margin no-padding">You better listen</p>
-        <h3>
-          words from the blessed.
-        </h3>
+        <h3>words from the blessed.</h3>
       </div>
 
-     <div className="testimonial-grid">
-         <TestimonialCard />
-         <TestimonialCard />
-         <TestimonialCard />
-         <TestimonialCard />
-         <TestimonialCard />
-         <TestimonialCard />
-     </div>
+      <div className="testimonial-grid">
+        {/* <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard />
+        <TestimonialCard /> */}
+
+        {testimonials?.map(testimonial => (
+          <TestimonialCard key={testimonial.id} testimonial={testimonial}/>
+        ))}
+      </div>
     </section>
   );
 }
